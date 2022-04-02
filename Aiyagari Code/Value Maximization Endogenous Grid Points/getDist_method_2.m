1f0c9338b03d765c38c2e5f679a251cb6d30c7b4
Oliko Vardishvili <48254304+ovardish@users.noreply.github.com>
2022-04-02 14:56:44 -0400

    cc

--- Aiyagari Code/Value Maximization Endogenous Grid Points/getDist_method_2.m
+++ Aiyagari Code/Value Maximization Endogenous Grid Points/getDist_method_2.m
@@ -22,32 +22,24 @@ function [dist,trans] = getDist_continuous(param,grid,kpol,trans_only)
 % % % find weights attached to point below/above policy
      varphi = (kpol-grid.k(ial))./(grid.k(ial+1)-grid.k(ial));
      varphi = min(varphi,1); varphi = max(varphi,0); % just to be safe, should not be binding
-% %     wbelow= 1-wabove;
-
-   
     
+ dist = NaN(param.nz,param.nkap);
     
-    dist = NaN(param.nz,param.nkap);
-   %if (trans_only~=1)
-
-     lambda     = ones(param.nz,param.nkap)/(param.nz*param.nkap);
-     lambda_new = zeros(param.nz,param.nkap);
-     err=1;
+ lambda     = ones(param.nz,param.nkap)/(param.nz*param.nkap);
+ lambda_new = zeros(param.nz,param.nkap);
+ err=1;
  while err>tiny
          
      for j=1:param.nz                % Current productivity state (in t)
          for i=1:param.nkap       % Current asset state (in t)
-          %  for j_p = 1:param.nz    % Future productivity state (in t+1)       
-           %   Asset state (in t+1)           
+               
            lambda_new(:,ial(j, i))=  lambda_new(:,ial(j, i))+grid.Pz(j,:)'.*        varphi(j, i)*lambda(j,i); 
            lambda_new(:,iar(j, i))=  lambda_new(:,iar(j, i))+grid.Pz(j,:)'.*(1.0d0-varphi(j, i))*lambda(j,i); 
-           % end
          end
        end
    err=max(abs(lambda_new(:)-lambda(:)));
    lambda=lambda_new;
    lambda_new = lambda_new.*0;
-   err
   end
    %end
 
