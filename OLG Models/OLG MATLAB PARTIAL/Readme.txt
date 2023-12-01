4ec359fcd341d1c84470c1594d51c48a197ed234
Oliko Vardishvili <oliko.vardishvili@yale.edu>
2023-12-01 10:28:04 -0800

    Add files via upload

---
+++ OLG Models/OLG MATLAB PARTIAL/Readme.txt
@@ -0,0 +1,5 @@
+Very beautiful code!
+
+* Just make eff automatic to age. as well as survival probabilities automatic to age, and use markovrapp to discretize productivity states.
+
+* Add definitions
---
+++ OLG Models/OLG MATLAB PARTIAL/linint_grow.m
@@ -0,0 +1,11 @@
+%% Linint Grow linint_grow(a_plus, a_l, a_u, a_grow, NA)
+function [il ir phi]=linint_grow(x, left, right, growth, n)
+if left>right     
+    display('linint_grow: left cannot be more than right');
+end
+
+
+
+end
+left=3
+right=22
